faf83a12f7278d97429914ba364be07d8120840e
sviatoslv <sviatoslav.boguslavskyi@gmail.com>
2020-04-23 08:28:02 +0300

    Refreshed master

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { TabNavigator } from './src/navigation/TabNavigator';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <TabNavigator />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
